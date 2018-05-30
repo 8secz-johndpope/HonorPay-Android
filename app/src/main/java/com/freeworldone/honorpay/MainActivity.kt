aeb3f35e072ed0ca72c6404a24a135e26fbbc4a6
Amagi82 <Amagi82@gmail.com>
2018-05-30 02:22:00 -0700

    initial commit

---
+++ app/src/main/java/com/freeworldone/honorpay/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.freeworldone.honorpay
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.navigation_home -> message.setText(R.string.title_home).run { true }
+                R.id.navigation_dashboard -> message.setText(R.string.title_dashboard).run { true }
+                R.id.navigation_notifications -> message.setText(R.string.title_notifications).run { true }
+                else -> false
+            }
+        }
+    }
+}
